02152a10c9cea481a2cf3c8c13323b3d7b2838f1
Atli Bjarnason <a@abjarna.com>
2024-06-11 01:06:50 +0000

    test

--- rit/conf.py
+++ rit/conf.py
@@ -266,7 +266,7 @@ html_sidebars = {
     "index": ["rubik-sidebar-nav-root", "rubik-icon-links"],
     "search": ["rubik-sidebar-nav-root", "rubik-icon-links"],
 }
-html_link_suffix = "/"
+html_link_suffix = ""
 html_show_copyright = False                     # Slökkt á default texta um höfundarrétt í HTML
 html_show_sphinx = False                        # Slökkt á "Created using Sphinx" texta í HTML
 
