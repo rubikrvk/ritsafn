2885b6a134920858c22d9b19d5bdf912c1739347
Atli Bjarnason <a@abjarna.com>
2024-06-12 03:02:53 +0000

    sphinx_copybutton update

---
+++ js/copybutton-is.js
@@ -0,0 +1,6 @@
+  'is': {
+    'copy': 'Afrita',
+    'copy_to_clipboard': 'Afrita í klippiborð',
+    'copy_success': 'Afritað!',
+    'copy_failure': 'Tókst ekki að afrita',
+  }
--- makesphinx.sh
+++ makesphinx.sh
@@ -4,6 +4,12 @@ cd rit
 make clean
 sphinx-build -b dirhtml . _build/html
 find _build/html/ -name "*.html" -exec prettier --config ../json/.prettierrc.json --write {} +
+TARGET_FILE="_build/html/_static/copybutton.js"
+SOURCE_FILE="../js/copybutton-is.js"
+NEW_CODE=$(cat "$SOURCE_FILE")
+sed -i '' "/const messages = {/r /dev/stdin" "$TARGET_FILE" << EOM
+$NEW_CODE,
+EOM
 make latexpdf
 mv _build/latex/*.pdf _build/html/
 for dir in */ ; do
