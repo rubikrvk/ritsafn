4465390e566fa5cd82c6f635d6b15d68432f9c5a
Atli Bjarnason <a@abjarna.com>
2024-06-27 23:20:57 +0000

    favicon test

--- sh/build-main-dirhtml-latexpdf.sh
+++ sh/build-main-dirhtml-latexpdf.sh
@@ -5,6 +5,10 @@ cd rit
 make clean
 sphinx-build -b dirhtml . _build/html
 
+# Fjarlægja "type" úr <link rel="icon" href="/favicon.ico" ... > og <link rel="apple-touch-icon" href="/apple-touch-icon.png" ... >
+find _build/html/ -name '*.html' -exec sed -i '' -e 's#<link rel="icon" href="/favicon.ico" sizes="32x32" type="image/x-icon" />#<link rel="icon" href="/favicon.ico" sizes="32x32">#' {} +
+find _build/html/ -name '*.html' -exec sed -i '' -e 's#<link rel="apple-touch-icon" href="/apple-touch-icon.png" type="image/png" />#<link rel="apple-touch-icon" href="/apple-touch-icon.png">#' {} +
+
 # Keyra Prettier á allar HTML skrár
 find _build/html/ -name "*.html" -exec prettier --config ../json/.prettierrc.json --write {} +
 
