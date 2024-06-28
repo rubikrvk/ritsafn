fdd31b02f4f90029c18a86de4c35842dc3ebca98
Atli Bjarnason <a@abjarna.com>
2024-06-28 03:37:21 +0000

    update <title> á forsíðu

--- sh/build-main-dirhtml-latexpdf.sh
+++ sh/build-main-dirhtml-latexpdf.sh
@@ -9,6 +9,9 @@ sphinx-build -b dirhtml . _build/html
 find _build/html/ -name '*.html' -exec sed -i '' -e 's#<link rel="icon" href="_static/favicon/favicon.ico" sizes="32x32"[^>]*>#<link rel="icon" href="_static/favicon/favicon.ico" sizes="32x32">#' {} +
 find _build/html/ -name '*.html' -exec sed -i '' -e 's#<link rel="apple-touch-icon" href="_static/favicon/apple-touch-icon.png"[^>]*>#<link rel="apple-touch-icon" href="_static/favicon/apple-touch-icon.png">#' {} +
 
+# Breyta <title> á forsíðu, með því að fjarlægja "Velkomin | "
+sed -i '' -e 's#<title>Velkomin | Ritsafn RÚBIK Reykjavíkur</title>#<title>Ritsafn RÚBIK Reykjavíkur</title>#' _build/html/index.html
+
 # Keyra Prettier á allar HTML skrár
 find _build/html/ -name "*.html" -exec prettier --config ../json/.prettierrc.json --write {} +
 
