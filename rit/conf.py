c7ea2de869e870d493b676a929423132c2349596
Atli Bjarnason <a@abjarna.com>
2024-06-30 02:15:59 +0000

    test2

--- rit/conf.py
+++ rit/conf.py
@@ -445,10 +445,10 @@ sitemap_url_scheme = "{link}"                               # Fjarlægja "/is" h
 # -- sphinx_favicon configuration --------------------------------------------
 
 favicons = [
-    {"rel": "icon", "href": "https://rit.rubik.is/_static/favicon/favicon.ico", "sizes": "32x32"},
-    {"rel": "icon", "href": "https://rit.rubik.is/_static/favicon/icon.svg", "type": "image/svg+xml"},
-    {"rel": "apple-touch-icon", "href": "https://rit.rubik.is/_static/favicon/apple-touch-icon.png"},
-    {"rel": "manifest", "href": "https://rit.rubik.is/_static/manifest.json"},
+    {"rel": "icon", "href": "https://rit-dev.rubik.is/_static/favicon/favicon.ico", "sizes": "32x32"},
+    {"rel": "icon", "href": "https://rit-dev.rubik.is/_static/favicon/icon.svg", "type": "image/svg+xml"},
+    {"rel": "apple-touch-icon", "href": "https://rit-dev.rubik.is/_static/favicon/apple-touch-icon.png"},
+    {"rel": "manifest", "href": "https://rit-dev.rubik.is/_static/manifest.json"},
 ]
 
 
