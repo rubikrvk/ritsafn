e51456ac84064dcf466c4c268b7ad7723cba03cf
Atli Bjarnason <a@abjarna.com>
2024-07-03 00:45:42 +0000

    layout.html update fyrir Google Tag Manager og favicon

--- sh/build-dev-html-latexpdf.sh
+++ sh/build-dev-html-latexpdf.sh
@@ -8,12 +8,11 @@ sphinx-build -b html -t dev . _build/html
 # Bæta "noindex, nofollow" við <head> í öllum HTML skrám fyrir DEV
 find _build/html/ -name '*.html' -exec sed -i '' -e 's/<head>/<head>\n<meta name="robots" content="noindex, nofollow">/' {} +
 
-# Breyta slóð á favicon í https://rit-dev.rubik.is/ fyrir DEV
+# Breyta https://rit.rubik.is/ í https://rit-dev.rubik.is/ fyrir DEV (m.a. fyrir favicon, canonical og mögulega fleira)
 find _build/html/ -name '*.html' -exec sed -i '' -e 's|href="https://rit.rubik.is/|href="https://rit-dev.rubik.is/|g' {} +
 
-# Fjarlægja Google Tag Manager kóða fyrir DEV
-find _build/html/ -name '*.html' -exec sed -i '' -e '/<script>(function(w,d,s,l,i){w\[l\]=w\[l\]\|\|\[\];w\[l\]\.push(/,/GTM-PL8JH23F/ d' {} \;
-find _build/html/ -name '*.html' -exec sed -i '' -e '/  <noscript><iframe src="https:\/\/www.googletagmanager.com\/ns.html?id=GTM-PL8JH23F"/,/<\/noscript>/d' {} \;
+# Breyta Google Tag Manager kóða fyrir DEV (úr "GTM-PL8JH23F" í "GTM-MLN5L94K")
+find _build/html/ -name '*.html' -exec sed -i '' -e 's|GTM-PL8JH23F|GTM-MLN5L94K|g' {} +
 
 # Breyta <title> á forsíðu, með því að fjarlægja "* &ndash; " (sem er skilgreint í "_templates/layout.html")
 sed -i '' -e 's#<title>.* &ndash; Ritsafn RÚBIK Reykjavíkur</title>#<title>Ritsafn RÚBIK Reykjavíkur</title>#' _build/html/index.html
